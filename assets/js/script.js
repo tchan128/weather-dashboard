48837fb6497e15c915c69eb882870e4788758794
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2023-11-09 00:51:14 -0500

    Added temp, wind, humidity, and date of future forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@ var city;
 var temperature;
 var wind;
 var humidity;
+var timeStamp = [6, 14, 22, 30, 38]
+
+function emojiAssign(condition) {
+    if (condition === "light snow") {
+    }
+}
 
 $('.search-btn').click(function(event){
     city = $("#userInput").val();
@@ -27,12 +33,52 @@ $('.search-btn').click(function(event){
         })
         .then(function (weather) {
             console.log(weather);
+            city = weather.city.name;
             temperature = weather.list[0].main.feels_like;
             wind = weather.list[0].wind.speed;
             humidity = weather.list[0].main.humidity;
-            city = weather.city.name;
+            code = weather.list[0].weather[0].icon;
+            var iconLink = `https://openweathermap.org/img/wn/${code}@2x.png`;
+            var weatherIcon = $("<img />");
+            weatherIcon.attr("src", iconLink);
 
             $(`#city`).text(`${city} (${dayjs().format("M/D/YYYY")})`);
+            // weatherIcon.insertAfter("#city");
+            $(`#temp`).text(`${temperature} ºF`);
+            $(`#wind`).text(`${wind} MPH`);
+            $(`#humidity`).text(`${humidity} %`);
+
+            for (var i = 0; i < timeStamp.length; i++) {
+                // Add Date
+                var dateEl = $("<h3></h3>");
+                var date = dayjs().add(i + 1, "d").format("M/D/YYYY");
+                dateEl.text(date);
+                $(`#day-${i}`).append(dateEl);
+
+                // Add icon
+                var weatherIcon = $("<img />");
+                weatherIcon.attr("src", iconLink);
+                $(`#day-${i}`).append(weatherIcon);
+
+                // Add temp
+                var tempEl = $("<h3></h3>");
+                var temperature = weather.list[timeStamp[i]].main.feels_like;
+                tempEl.text(`Temp: ${temperature} ºF`);
+                $(`#day-${i}`).append(tempEl);
+
+                // Add Wind
+                var windEl = $("<h3></h3>");
+                var wind = weather.list[timeStamp[i]].wind.speed;
+                windEl.text(`Wind: ${wind} MPH`);
+                $(`#day-${i}`).append(windEl);
+
+                // Add Humidity
+                var humidityEl = $("<h3></h3>");
+                var humidity = weather.list[timeStamp[i]].main.humidity;
+                humidityEl.text(`Wind: ${humidity} MPH`);
+                $(`#day-${i}`).append(humidityEl);
+                
+            }
         })
     });
 })
